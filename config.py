cc1cae4b226a7fbaaed6d341c56b5ea68428634d
insmnia <64920575+insmnia@users.noreply.github.com>
2020-06-18 21:13:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,1 @@
-TOKEN = '1134726122:AAEOAejn675GYVF9MhkBANkZU4hc1qvoI74'
-
-"""{"message_id": 6, "from": {"id": 499114209, "is_bot": false, "first_name": "Евклид", "last_name": "Шуруповёртов", "username": "Kei_dan",
-"language_code": "ru"}, "chat": {"id": 499114209, "first_name": "Евклид", "last_name": "Шуруповёртов", "username": "Kei_dan",
-"type": "private"}, "date": 1590513668, "text": "/start", "entities": [{"offset": 0, "length": 6, "type": "bot_command"}]}
-
-
-{"id": "2143679208777079410", "from": {"id": 499114209, "is_bot": false, "first_name": "Евклид", "last_name": "Шуруповёртов",
-"username": "Kei_dan", "language_code": "ru"}, "message": {"message_id": 416, "from": {"id": 1134726122, "is_bot": true,
-"first_name": "VQfamBY", "username": "VQfamBY_bot"}, "chat": {"id": 499114209, "first_name": "Евклид", "last_name": "Шуруповёртов",
-"username": "Kei_dan", "type": "private"}, "date": 1590764493,
-"text": "Ваше имя: a\nВаша дата рождения: a\nВаша машина: a\nВаш номер машины: a", "entities": [{"offset": 0, "length": 9, "type": "bold"},
- {"offset": 12, "length": 19, "type": "bold"}, {"offset": 34, "length": 12, "type": "bold"}, {"offset": 49, "length": 17, "type": "bold"}],
-  "reply_markup": {"inline_keyboard": [[{"text": "Да", "callback_data": "yes"}], [{"text": "Нет", "callback_data": "no"}]]}},
-  "chat_instance": "1767436744095769033", "data": "no"}
-
-"""
+TOKEN = ""
